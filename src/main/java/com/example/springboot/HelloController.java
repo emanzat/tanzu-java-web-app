f84a3303b2213b6659fc142424fdae754c78f6a7
emanzat <emanzat@users.noreply.github.com>
2024-02-20 15:16:27 +0100

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu janvier!";
+		return "Greetings from Spring Boot + Tanzu fevrier 2024!";
 	}
 
 }
